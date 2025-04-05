a6de90bc21313c34ccbbc1ed9612d195f7c19e46
SebasProgram777 <juanpro15m@gmail.com>
2025-04-04 19:36:59 -0500

    archivos imprimir e ingreso

---
+++ Program.py
@@ -0,0 +1,4 @@
+print( 2**4 )   
+
+c = "Estudia" * 5
+print (c)
---
+++ imprimir.py
@@ -0,0 +1,12 @@
+a = "Carlos"
+b = 20
+print ("Hola ", a, "tienes ", b)
+
+c ="Hola " + str (a) + "tienes" + str (b)
+print (c) 
+
+d = f"Hola {a} tienes {b}" 
+print (d)
+
+#comentario una linea 
+
